d14741de2834b2a7e8c335589f2eabd1bd6ec7c0
DamienQ <95469825+querbesd@users.noreply.github.com>
2024-05-29 12:01:53 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='inductive_clusterer',
+    version='0.1.0',
+    url='https://github.com/querbesd/public_code',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scikit-learn=1.0.2',
+        'scipy'
+    ],
+)
