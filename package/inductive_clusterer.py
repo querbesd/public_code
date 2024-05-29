e75fc3132c3976b2aca0955c6472e9d79ff2290d
DamienQ <95469825+querbesd@users.noreply.github.com>
2024-05-29 16:41:22 +0200

    Update inductive_clusterer.py

--- package/inductive_clusterer.py
+++ package/inductive_clusterer.py
@@ -10,11 +10,9 @@ class InductiveClusterer(BaseEstimator, ClusterMixin, ClassifierMixin):
         self.previous_labels = previous_labels
 
     def fit(self, X, y=None):
-        logging.info(f"Starting fit method. Data shape: {X.shape}")
         self.clusterer_ = clone(self.clusterer)
         self.classifier_ = clone(self.classifier)
         y_pred = self.clusterer_.fit_predict(X)
-        logging.info(f"Cluster labels predicted: {y_pred}")
 
         if self.previous_labels is not None:
             y_pred = self._align_labels(y_pred, self.previous_labels)
@@ -23,15 +21,12 @@ class InductiveClusterer(BaseEstimator, ClusterMixin, ClassifierMixin):
         return self
 
     def predict(self, X):
-        logging.info(f"Starting predict method. Data shape: {X.shape}")
         return self.classifier_.predict(X)
 
     def decision_function(self, X):
-        logging.info(f"Starting decision_function method. Data shape: {X.shape}")
         return self.classifier_.decision_function(X)
     
     def _align_labels(self, new_labels, previous_labels):
-        logging.info("Aligning labels.")
         cost_matrix = np.zeros((len(np.unique(previous_labels)), len(np.unique(new_labels))))
 
         for i in range(len(np.unique(previous_labels))):
@@ -41,7 +36,6 @@ class InductiveClusterer(BaseEstimator, ClusterMixin, ClassifierMixin):
         row_ind, col_ind = linear_sum_assignment(cost_matrix, maximize=True)
         mapping = {new_label: old_label for new_label, old_label in zip(col_ind, row_ind)}
         aligned_labels = np.vectorize(mapping.get)(new_labels)
-        logging.info(f"Aligned labels: {aligned_labels}")
         return aligned_labels
 
     def fit_predict(self, X, y=None):
@@ -49,12 +43,10 @@ class InductiveClusterer(BaseEstimator, ClusterMixin, ClassifierMixin):
         return self.predict(X)
 
     def save_model(self, path):
-        logging.info(f"Saving model to {path}")
         with open(path, "wb") as f:
             joblib.dump(self, f)
 
     @classmethod
     def load_model(cls, path):
-        logging.info(f"Loading model from {path}")
         with open(path, "rb") as f:
             return joblib.load(f)
